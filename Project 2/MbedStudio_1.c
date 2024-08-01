71d59b80c56dd3e2bd739483902783e70bd4c1bc
Berkant-Simsek <126967154+Berkant-Simsek@users.noreply.github.com>
2024-08-01 14:37:02 +0300

    Update MbedStudio_1.c

--- Project 2/MbedStudio_1.c
+++ Project 2/MbedStudio_1.c
@@ -8,50 +8,49 @@ DigitalOut led2(PB_5);
 volatile int counter = 0; 
 uint32_t elapsedTime; 
 Timer timer; 
+
 void buttonRise(){ 
-counter++; 
+ counter++; 
 } 
+
 int main() { 
-led1 = 1; 
-led2 = 1; 
-button.rise(&buttonRise); 
-t
- imer.start(); 
-while(1) { 
-elapsedTime = 
-chrono::duration_cast<chrono::milliseconds>(timer.elapsed_time()).count(); 
-if (elapsedTime > 2000) { 
-printf("Timer: %d\n",elapsedTime); 
-t
- imer.reset(); 
-            if (counter == 3) { 
-                printf("3 kere basildi\n"); 
+ led1 = 1; 
+ led2 = 1; 
+ button.rise(&buttonRise); 
+ timer.start(); 
+ while(1) { 
+  elapsedTime = chrono::duration_cast<chrono::milliseconds>(timer.elapsed_time()).count(); 
+  if (elapsedTime > 2000) { 
+   printf("Timer: %d\n",elapsedTime); 
+   timer.reset(); 
+   if (counter == 3) { 
+    printf("3 kere basildi\n"); 
                  
-                for (int i = 0; i < 2; i++) { 
-                    led1 = !led1; 
-                    thread_sleep_for(500); 
-                    led1 = !led1; 
-                    thread_sleep_for(500); 
-                } 
-                counter = 0; 
-            }  
- 
-            else if (counter == 5) { 
-                printf("5 kere basildi\n"); 
- 
-                for (int i = 0; i < 4; i++) { 
-                    led2 = !led2; 
-                    thread_sleep_for(500); 
-                    led2 = !led2; 
-                    thread_sleep_for(500); 
-                } 
-                counter = 0; 
-            } 
-            counter = 0; 
-        } 
- 
-        else { 
-            counter = 0; 
-        } 
+    for (int i = 0; i < 2; i++) { 
+     led1 = !led1; 
+     thread_sleep_for(500); 
+     led1 = !led1; 
+     thread_sleep_for(500); 
     } 
+    counter = 0; 
+   }  
+ 
+   else if (counter == 5) { 
+    printf("5 kere basildi\n"); 
+ 
+    for (int i = 0; i < 4; i++) { 
+     led2 = !led2; 
+     thread_sleep_for(500); 
+     led2 = !led2; 
+     thread_sleep_for(500); 
+    } 
+    counter = 0; 
+   } 
+   counter = 0; 
+  } 
+ 
+  else { 
+   counter = 0; 
+  } 
+ } 
 } 
